d08dc3e8b3e6a5f19f6097194705ccc8109f31c7
NIKITA.MARKOV <nikita.markov@vkpay.com>
2021-07-19 21:41:53 +0300

    task-2: Utils functions (slice of batches, match reverse and filter by hardcoded values) were added.

---
+++ internals/utils/utils.go
@@ -0,0 +1,66 @@
+package utils
+
+import (
+	"errors"
+	"math"
+)
+
+func MakeSliceOfBatches(inSlice []int, batchLen int) ([][]int, error) {
+	inSliceLen := len(inSlice)
+	if batchLen < 1 {
+		return nil, errors.New("batch len can't be less than 1")
+	}
+
+	if inSliceLen < batchLen {
+		return nil, errors.New("len of the slice can't be less than batch len")
+	}
+
+	regularBatchesNumber := inSliceLen / batchLen
+
+	batchNumber := int(math.Ceil(float64(inSliceLen) / float64(batchLen)))
+	outSlice := make([][]int, 0, batchNumber)
+
+	for i := 0; i < regularBatchesNumber; i++ {
+		offset := i * batchLen
+		outSlice = append(outSlice, inSlice[offset:offset+batchLen])
+	}
+
+	if regularBatchesNumber != batchNumber {
+		outSlice = append(outSlice, inSlice[regularBatchesNumber*batchLen:])
+	}
+
+	return outSlice, nil
+}
+
+func ReverseMap(inMap map[string]int) (map[int][]string, error) {
+	outMap := make(map[int][]string)
+
+	for k, v := range inMap {
+		outMap[v] = append(outMap[v], k)
+	}
+
+	return outMap, nil
+}
+
+func FilterByHardcodedValues(inSlice []int) ([]int, error) {
+	hardcodedValues := map[int]bool{1: true, 2: true, 3: true}
+
+	lenInSlice := len(inSlice)
+	if lenInSlice == 0 {
+		return make([]int, 0), nil
+	}
+
+	sliceCapacity := len(inSlice) - len(hardcodedValues)
+	if sliceCapacity < 0 {
+		sliceCapacity = len(inSlice)
+	}
+
+	outSlice := make([]int, 0, sliceCapacity)
+	for _, v := range inSlice {
+		if _, ok := hardcodedValues[v]; !ok {
+			outSlice = append(outSlice, v)
+		}
+	}
+
+	return outSlice, nil
+}
